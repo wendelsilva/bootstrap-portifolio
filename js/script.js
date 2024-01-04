f391d40ff3e1d7f672bcd08732a9de9b629e9dbc
wendel <wendel245.ws@gmail.com>
2024-01-03 21:25:39 -0300

    create a new header and added new theme color

--- js/script.js
+++ js/script.js
@@ -1,34 +1,11 @@
-function toggleMenu() {
-    var menuButton = document.querySelector('.menu-button');
-    var menu = document.querySelector('.menu')
-    menuButton.addEventListener('click', () => {
-        if(!menu.classList.contains('closed')) {
-            menu.classList.remove('slide-bottom');
-            menu.classList.add('slide-top');
-            menu.classList.add('closed');
-            menuButton.style.filter = 'brightness(1)';
-        } else {
-            menu.classList.remove('slide-top');
-            menu.classList.add('slide-bottom');
-            menu.classList.remove('closed');
-            menuButton.style.filter = 'brightness(10)';
-        }
-    })
-}
+function toggleSideMenu() {
+    const sideMenu = document.querySelector(".side-menu");
 
-function closeMenuNavigate() {
-    var menuButton = document.querySelector('.menu-button');
-    var menu = document.querySelector('.menu')
-    var menuItems = menu.querySelectorAll('a')
-    menuItems.forEach(item => {
-        item.addEventListener('click', () => {
-            menu.classList.remove('slide-bottom');
-            menu.classList.add('slide-top');
-            menu.classList.add('closed');
-            menuButton.style.filter = 'brightness(1)';
-        })
-    })
-}
+    sideMenu.classList.toggle("open")
 
-toggleMenu();
-closeMenuNavigate();
+    if(sideMenu.classList.contains("open")) {
+        document.querySelector(".menu-button").setAttribute("src", "./assets/icons/close.svg")
+    } else {
+        document.querySelector(".menu-button").setAttribute("src", "./assets/icons/list.svg")
+    }
+}
